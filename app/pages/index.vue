617c21ef9a4c3cf76c5be22fe18ee0e652afcd73
ohmmaagoch <phatcharaphol@hotmail.com>
2024-08-31 02:21:15 +0700

    feat: add simple fetch user

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,33 @@
 <template>
   <div class="container">
-    <h1>hello default page</h1>
+    <h1>User list</h1>
+     <card v-for="user in users" :key="user._id" :user="user" />
   </div>
 </template>
+
+<script setup lang="ts">
+import axios from 'axios'
+import { ref } from 'vue'
+import type  { TUser }  from '~/types/user-type'
+
+
+const users: TUser[] = ref<TUser[]>([])
+
+const fetchUsers = async () => {
+  try {
+    const response = await axios.get(`${import.meta.env.VITE_API_URL}/users`)
+    const data = await response.data
+    users.value = response.data
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+onMounted(() => fetchUsers())
+
+
+
+
+
+
+</script>
---
+++ app/types/user-type.ts
@@ -0,0 +1,8 @@
+export type TUser = {
+    _id:          string;
+    username:     string;
+    email:        string;
+    age:          number;
+    middle_name?: string;
+    __v?:         number;
+}
