19d1edf265c369163f308eb651b6a09edccb2d40
ohmmaagoch <phatcharaphol@hotmail.com>
2024-09-02 23:18:10 +0700

    feat: add navbar

---
+++ app/layouts/default.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Navbar theme="dark" expand="lg" background-color="body-tertiary">
+      <Container type="fluid">
+        <NavbarBrand href="/">Logo</NavbarBrand>
+        <NavbarToggler />
+        <NavbarCollapse>
+          <NavbarNavList>
+            <NavItem>
+              <NavLink active aria-current="page" href="/">user</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink active aria-current="page" href="/job">job</NavLink>
+            </NavItem>
+          </NavbarNavList>
+        </NavbarCollapse>
+      </Container>
+    </Navbar>
+    <slot></slot>
+  </div>
+</template>
+<script></script>
